47d6511366f24675975fc6bdd8bc5a0ad2f3aa74
Nora Kauczor <n.kauczor@web.de>
2024-03-22 15:42:16 +0100

    add fetch function

--- components/CharacterCard/CharacterCard.js
+++ components/CharacterCard/CharacterCard.js
@@ -1,33 +1,26 @@
-export function CharacterCard() {
-  const cardContainer = document.querySelector('[data-js="card-container"]');
-  console.log(cardContainer);
-
+export function CharacterCard(characterData) {
   const card = document.createElement("li");
-  card.classList.add(".card");
+  card.classList.add("card");
+
   card.innerHTML = `<div class="card__image-container">
         <img
           class="card__image"
-          src="https://rickandmortyapi.com/api/character/avatar/1.jpeg"
-          alt="Rick Sanchez"
+          src="${characterData.image}"
+          alt="${characterData.name}"
         />
         <div class="card__image-gradient"></div>
       </div>
       <div class="card__content">
-    // name
-        <h2 class="card__title">Rick Sanchez</h2>
+        <h2 class="card__title">${characterData.name}</h2>
         <dl class="card__info">
           <dt class="card__info-title">Status</dt>
-          <dd class="card__info-description">Alive</dd>
+          <dd class="card__info-description">${characterData.status}</dd>
           <dt class="card__info-title">Type</dt>
-          <dd class="card__info-description"></dd>
+          <dd class="card__info-description">${characterData.type}</dd>
           <dt class="card__info-title">Occurrences</dt>
-          <dd class="card__info-description">51</dd>
+          <dd class="card__info-description">${characterData.episode.length}</dd>
         </dl>
       </div>`;
-
-  const name = document.createElement("h2");
-
-  cardContainer.append(card);
 
   return card;
 }
