ea532dd4d48344cb101cfa759d231d75f4adeb29
Larissa Sartori-Cader <sartorilarissa.br@gmail.com>
2024-03-22 11:46:01 +0100

    add new carachter card

--- components/CharacterCard/CharacterCard.js
+++ components/CharacterCard/CharacterCard.js
@@ -1,1 +1,29 @@
-export function CharacterCard() {}
+export function CharacterCard() {
+  const cardContainer = document.querySelector('[data-js="card-container"]');
+  console.log(cardContainer);
+
+  const card = document.createElement("li");
+  card.classList.add(".card");
+  card.innerHTML = `<div class="card__image-container">
+        <img
+          class="card__image"
+          src="https://rickandmortyapi.com/api/character/avatar/1.jpeg"
+          alt="Rick Sanchez"
+        />
+        <div class="card__image-gradient"></div>
+      </div>
+      <div class="card__content">
+        <h2 class="card__title">Rick Sanchez</h2>
+        <dl class="card__info">
+          <dt class="card__info-title">Status</dt>
+          <dd class="card__info-description">Alive</dd>
+          <dt class="card__info-title">Type</dt>
+          <dd class="card__info-description"></dd>
+          <dt class="card__info-title">Occurrences</dt>
+          <dd class="card__info-description">51</dd>
+        </dl>
+      </div>`;
+  cardContainer.append(card);
+
+  return card;
+}
